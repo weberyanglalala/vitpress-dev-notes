88bc7f7efe7a5b9807edb7328de8683be4123ede
Weber Yang <weberyanglalala@gmail.com>
2024-03-26 22:43:11 +0800

    doc(vitpress): add vitpress to nav and sidebar

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitepress'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -10,33 +10,39 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: '.NET', link: '/dotnet/index' }
+      {text: 'Home', link: '/'},
+      {text: '.NET', link: '/dotnet/index'},
+      {text: 'VitPress', link: '/vitpress/index'}
     ],
-
     sidebar: [
       {
         text: '.NET',
         items: [
-          { text: 'Coravel 範例', link: '/dotnet/coravel' },
+          {text: 'Coravel 範例', link: '/dotnet/coravel'},
         ]
       },
       {
         text: 'Vue',
         items: [
-          { text: 'Index', link: '/vue/index' },
+          {text: 'Index', link: '/vue/index'},
         ]
       },
       {
         text: 'Semantic Kernel',
         items: [
-          { text: 'Index', link: '/semantic-kernel/index' },
+          {text: 'Index', link: '/semantic-kernel/index'},
         ]
-      }
+      },
+      {
+        text: 'VitPress',
+        items: [
+          {text: '部署流程', link: '/vitpress/index'},
+        ]
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
     ]
   }
 })
