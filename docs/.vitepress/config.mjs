f7165243f5d6fd60a84d3dd2699f749127f3e98d
Weber Yang <weberyanglalala@gmail.com>
2024-04-19 12:14:31 +0800

    add hotjar

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -58,6 +58,20 @@ export default withMermaid({
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
       gtag('config', 'G-B7LWF9VCBK');`
-    ]
+    ],
+    [
+      'script',
+      {},
+      `<!-- Hotjar Tracking Code for VitPress -->
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:4950929,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+    ],
+
   ]
 })
